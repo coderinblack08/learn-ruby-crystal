67d8287f4e92e4beea15ba847992579af90eb7f4
coderinblack <kevinlu.email@gmail.com>
2021-05-11 19:18:34 -0700

    Functions and classes

---
+++ functions.rb
@@ -0,0 +1,24 @@
+def hello(name = "Jeff")
+  puts "Hello World #{name.capitalize}"
+end
+
+hello
+hello "kevin"
+hello("thomas")
+
+class Greeter
+  def initialize(name = "World")
+    # instance variable
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}"
+  end
+  def say_bye
+    puts "Bye #{@name}"
+  end
+end
+
+greeter = Greeter.new("Pat")
+greeter.say_hi
+greeter.say_bye
---
+++ math.rb
@@ -0,0 +1,3 @@
+puts 1 + 2 + 3, 1 - 2 + 3
+puts Math.cos(1), Math.sin(1), Math.tan(1)
+puts Math.sqrt(16), 4**2
